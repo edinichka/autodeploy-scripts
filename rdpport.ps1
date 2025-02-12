91db2a74e817e1ffad60d4a15f8df46207d47ad5
edinichka <33804713+edinichka@users.noreply.github.com>
2025-02-12 20:13:17 +0200

    uncomment

--- rdpport.ps1
+++ rdpport.ps1
@@ -1,9 +1,14 @@
 # write new rdp port
 $port = Read-Host "Type new port"
-#reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d $port /f
-#netsh advfirewall firewall add rule name="RDP Custom Port" dir=in action=allow protocol=TCP localport=$port
-#netsh advfirewall firewall add rule name="RDP Custom Port" dir=in action=allow protocol=UDP localport=$port
-#net stop TermService /y
-#net start TermService
+# deploing new port into Registry
+reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d $port /f
+# add firewall accepting TCP rule
+netsh advfirewall firewall add rule name="RDP Custom Port" dir=in action=allow protocol=TCP localport=$port
+# add firewall accepting UDP rule
+netsh advfirewall firewall add rule name="RDP Custom Port" dir=in action=allow protocol=UDP localport=$port
+# reload Terminal Service
+net stop TermService /y
+net start TermService
+# Well Done.
 Write-Host "RDP port has been changed. New RDP is $port"
 Read-Host "Press Enter for exit."
