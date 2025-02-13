c885fc8899693b6b2dcc049620eeec02c36008b4
edinichka <33804713+edinichka@users.noreply.github.com>
2025-02-13 18:53:49 +0200

    Update AutoDeployBFB.ps1

--- AutoDeployBFB.ps1
+++ AutoDeployBFB.ps1
@@ -1,21 +1,28 @@
-# variables
+# змінні
 $downloadUrl = "https://github.com/edinichka/autodeploy-rdp-custom-port/raw/refs/heads/main/BFGuard.ps1"
-$scriptPath = "C:\Scripts\BFGuard.ps1"           # ???? ???????? ??? ??????
+$scriptPath = "C:\Scripts\BFGuard.ps1"           # Шлях до скрипта
 $taskName = "AutoBlockBruteForce"
 
-# create dir
+# створити директорію
 if (!(Test-Path "C:\Scripts")) {
     New-Item -ItemType Directory -Path "C:\Scripts"
 }
 
-# download script
+# завантажити скрипт
 Invoke-WebRequest -Uri $downloadUrl -OutFile $scriptPath
 
-# create 
+# створити дію
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
-$trigger = New-ScheduledTaskTrigger -At (Get-Date).AddMinutes(1) -RepetitionInterval (New-TimeSpan -Minutes 10) -RepetitionDuration (New-TimeSpan -Days 3650)
+
+# створити тригер для запуску кожні 10 хвилин, починаючи з поточного часу
+$trigger = New-ScheduledTaskTrigger -Daily -At (Get-Date).AddMinutes(1)
+$trigger.RepetitionInterval = New-TimeSpan -Minutes 10
+$trigger.RepetitionDuration = New-TimeSpan -Days 3650
+
+# налаштування
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -AllowHardTerminate -MultipleInstances IgnoreNew
 
+# зареєструвати заплановану задачу
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -RunLevel Highest -Force
 
-Write-Output "Task $taskName succesfully created, script was downloaded and deployed in $scriptPath"
+Write-Output "Задача $taskName успішно створена, скрипт був завантажений і розгорнутий за адресою $scriptPath"
