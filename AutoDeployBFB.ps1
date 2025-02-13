29b39aee2b83d663f8bf917535f21666a5373591
edinichka <33804713+edinichka@users.noreply.github.com>
2025-02-13 18:56:51 +0200

    Update AutoDeployBFB.ps1

--- AutoDeployBFB.ps1
+++ AutoDeployBFB.ps1
@@ -15,12 +15,12 @@ Invoke-WebRequest -Uri $downloadUrl -OutFile $scriptPath
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
 
 # створити тригер для запуску кожні 10 хвилин, починаючи з поточного часу
-$trigger = New-ScheduledTaskTrigger -Daily -At (Get-Date).AddMinutes(1)
+$trigger = New-ScheduledTaskTrigger -At (Get-Date).AddMinutes(1)
 $trigger.RepetitionInterval = New-TimeSpan -Minutes 10
 $trigger.RepetitionDuration = New-TimeSpan -Days 3650
 
-# налаштування
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -AllowHardTerminate -MultipleInstances IgnoreNew
+# налаштування (без AllowHardTerminate)
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -MultipleInstances IgnoreNew
 
 # зареєструвати заплановану задачу
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -RunLevel Highest -Force
